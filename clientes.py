4951220c23647bc199835d533c13e5123d2d9a5a
sadikibra <sgibrahim123@gmail.com>
2022-12-28 23:11:32 +0000

    Introduzida a feature que permite criar clientes

---
+++ clientes.py
@@ -0,0 +1,19 @@
+from io_terminal import imprime_lista
+
+nome_ficheiro_lista_de_clientes = "lista_de_cliente.pk"
+
+def cria_novo_cliente():
+    """ pede os dados de um novo cliente
+
+    :return: dicionario com o novo utilizador, {"nome": <<nome>>, "email": <<email>>}
+    """
+    nome = input("nome? ")
+    email = input("email? ")
+    return {"nome": nome, "email": email}
+
+
+def imprime_lista_de_clientes(lista_de_clientes):
+    """ ..."""
+   
+    imprime_lista(cabecalho="Lista de Cliente", lista=lista_de_clientes)
+    
--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from espetaculos import (
     cria_espetaculos,
     imprime_lista_de_espetaculos,
 )
-
+from clientes import (
+    cria_novo_cliente,
+    imprime_lista_de_clientes,
+)
 from lotacao import(
     lotacao_salas
 )
 def menu():
     """ Funcao menu cria o menu da aplicacao"""
     lista_de_espetaculos = []
-  
+    lista_de_clientes = []
 
     while True:
         print("""
@@ -42,6 +45,11 @@ def menu():
             lista_de_espetaculos.append(nova_espetaculos)
         elif op == "el":
             imprime_lista_de_espetaculos(lista_de_espetaculos)
+        elif op == "cn":
+            novo_cliente = cria_novo_cliente()
+            lista_de_clientes.append(novo_cliente)
+        elif op == "cl":
+            imprime_lista_de_clientes(lista_de_clientes)
         #to do: implementar novas features
 
 if __name__ == "__main__":
