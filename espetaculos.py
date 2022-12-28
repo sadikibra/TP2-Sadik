2c3911e6a797fa7ca5ea17b274420b86c0533880
sadikibra <sgibrahim123@gmail.com>
2022-12-28 23:00:19 +0000

    Introduzidas as features de criacao de espetaculos

---
+++ espetaculos.py
@@ -0,0 +1,18 @@
+from io_terminal import imprime_lista
+
+def cria_espetaculos():
+    """ Pede ao utilizador para introduzir uma novo espetaculos
+
+    :return: dicionario com um espetaculo na forma 
+        {"marca": <<marca>>, "modelo": <<modelo>>, ... COMPLETAR ...}
+    """
+
+    nome_espetaculo = input("nome? ")
+    local_espetaculo = input("local? ").upper()
+    return {"nome": nome_espetaculo, "local": local_espetaculo}
+ 
+
+def imprime_lista_de_espetaculos(lista_de_espetaculos):
+    """ ..."""
+
+    imprime_lista(cabecalho="Lista de Espetaculos", lista=lista_de_espetaculos)
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+
+from espetaculos import (
+    cria_espetaculos,
+    imprime_lista_de_espetaculos,
+)
+
 from lotacao import(
     lotacao_salas
 )
 def menu():
     """ Funcao menu cria o menu da aplicacao"""
-
+    lista_de_espetaculos = []
   
 
     while True:
@@ -31,6 +37,11 @@ def menu():
             exit()
         elif op == "ls":
             lotacao_salas()
+        elif op == "en":
+            nova_espetaculos = cria_espetaculos()
+            lista_de_espetaculos.append(nova_espetaculos)
+        elif op == "el":
+            imprime_lista_de_espetaculos(lista_de_espetaculos)
         #to do: implementar novas features
 
 if __name__ == "__main__":
